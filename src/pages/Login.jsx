5361f2c7c750a19ba5cfd9c91762a1e26a789a3b
Rafael Ribeiro <rafael.rrf@hotmail.com>
2023-03-02 20:34:24 -0300

    Criação e validação dos inputs, salvando email no LS - Req 2,3,4,5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const validation = () => {
+    const min = 6;
+    const emailRegex = /^\S+@\S+\.\S+$/i.test(userEmail);
+    if (emailRegex && userPassword.length >= min) {
+      setIsDisabled(false);
+      console.log('Botão válido');
+    } else { setIsDisabled(true); }
+  };
   return (
-    <div>Login</div>
+    <div>
+      <input
+        type="text"
+        placeholder="EMAIL"
+        data-testid="email-input"
+        onChange={ ({ target }) => {
+          setUserEmail(target.value);
+          validation();
+        } }
+      />
+      <input
+        type="password"
+        placeholder="SENHA"
+        data-testid="password-input"
+        onChange={ ({ target }) => {
+          setUserPassword(target.value);
+          validation();
+        } }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        onClick={ () => localStorage.setItem('user', JSON
+          .stringify({ email: userEmail })) }
+        disabled={ isDisabled }
+      >
+        Logar
+      </button>
+
+    </div>
   );
 }
 
