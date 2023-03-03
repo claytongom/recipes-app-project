b4298e2c371e221db6519d8e8f76a168caf9bab0
GabrielSilper <gabrielsilper.main@gmail.com>
2023-03-02 20:10:41 -0400

    Tela de Perfil - elemento email e botões ok, teste 100%

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Profile() {
+  // Vou alimentar o estado email com o que tem no localStorage segundo o requisito.
+  const [email, setEmail] = useState('');
+  const history = useHistory();
+
+  // Só pra colocar algo no local storage
+  // localStorage.setItem("user", JSON.stringify({ email: "email@email.com" }));
+
+  // UseEffect verifica se a chave existe e reatribui o estado email com o valor.
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      const user = JSON.parse(localStorage.getItem('user'));
+      setEmail(user.email);
+    }
+  }, []);
+
+  // Função para o Botão logout
+  const DoLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
-    <div>Profile</div>
+    <div>
+      <h1>Profile</h1>
+      <p data-testid="profile-email">{email}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ DoLogout }
+      >
+        Logout
+      </button>
+    </div>
   );
 }
