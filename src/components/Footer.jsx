aeb8e4a7b786cef9a30bfca4896f5e9ab9d95635
Lucas <lm.spizzirri@gmail.com>
2023-03-03 15:06:08 -0300

    Componente Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import mealIcon from '../images/mealIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <Link to="/meals">
+        <img src={ mealIcon } alt="Meal Icon" data-testid="meals-bottom-btn" />
+      </Link>
+      <Link to="/drinks">
+        <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn" />
+      </Link>
+    </footer>
   );
 }
