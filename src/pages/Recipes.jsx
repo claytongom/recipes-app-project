4093dc7b3973c1f663ca5e3feb625a4283800ac6
lucaslsx21 <lucaslsilva@live.com>
2023-03-03 22:33:09 -0300

    (Feat) initial recipes

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import Footer from '../components/Footer';
+import PropTypes from 'prop-types';
 
-export default function Recipes() {
+function Recipes(props) {
+  const { recipe, isLoading } = props;
+
+  const MAX_LENGTH = 12;
+  if (isLoading) return 'Carregando';
+
   return (
-    <div>
-      <Footer />
-    </div>
+    <main>
+      {
+        recipe.map((item, index) => (
+          <div
+            key={ item.idDrink || item.idMeal }
+            data-testid={ `${index}-recipe-card` }
+          >
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ item.strDrinkThumb || item.strMealThumb }
+              alt="foto do produto"
+            />
+            <p
+              data-testid={ `${index}-card-name` }
+            >
+              {item.strMeal || item.strDrink}
+
+            </p>
+          </div>
+        )).slice(0, MAX_LENGTH)
+      }
+    </main>
   );
 }
+
+Recipes.propTypes = {
+  recipe: PropTypes.arrayOf().isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
+export default Recipes;
