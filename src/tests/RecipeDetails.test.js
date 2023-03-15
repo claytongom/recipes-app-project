fc5de1d2e68d4d8fbad384c9d7fd7750f64afaab
Rafael Ribeiro <rafael.rrf@hotmail.com>
2023-03-15 00:25:42 -0300

    Realização de parte dos testes da página de recipe details

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -1,22 +1,91 @@
+import { screen, waitFor } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
 import renderWithRouterAndContextProvider from './helpers/renderWithRouterAndContextProvider';
 import App from '../App';
 import fetch from '../../cypress/mocks/fetch';
-import { act } from 'react-dom/test-utils';
-import { screen, waitFor } from '@testing-library/react';
 
+const endPoint = '/drinks/178319';
 describe('Teste da página Recipe Details', () => {
   beforeEach(async () => {
     global.fetch = jest.fn(fetch);
   });
 
-  test('should first', async () => {
+  test('Verifica se as informações sobre a receita "Aquamarine" é renderizada na tela', async () => {
     const { history } = renderWithRouterAndContextProvider(<App />);
     act(() => {
-      history.push('/drinks/178319');
+      history.push(endPoint);
     });
 
     await waitFor(() => {
-      screen.getByText('Aquamarine');
+      expect(screen.getByText('Aquamarine')).toBeInTheDocument();
     });
+    expect(screen.getByText('Ingredients')).toBeInTheDocument();
+    expect(screen.getByText('Category')).toBeInTheDocument();
+    expect(screen.getByText('Intructions')).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: /https:\/\/www\.thecocktaildb\.com\/images\/media\/drink\/zvsre31572902738\.jpg/i })).toBeInTheDocument();
+    expect(screen.getByText(/alcoholic/i)).toBeInTheDocument();
   });
+
+  test('Verifica se as informações de recomendações estão sendo renderizadas na tela', async () => {
+    const { history } = renderWithRouterAndContextProvider(<App />);
+    act(() => {
+      history.push(endPoint);
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText(/corba/i)).toBeInTheDocument();
+    });
+    expect(screen.getByRole('img', { name: /corba/i })).toBeInTheDocument();
+  });
+
+  test('Verifica se os botões estão sendo renderizados na tela', async () => {
+    const { history } = renderWithRouterAndContextProvider(<App />);
+    act(() => {
+      history.push(endPoint);
+    });
+
+    await waitFor(() => {
+      expect(screen.getByTestId('favorite-btn')).toBeInTheDocument();
+    });
+    expect(screen.getByTestId('share-btn')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /share/i }));
+    screen.logTestingPlaygroundURL();
+  });
+
+  test('Verifica se ao clicar no botão "Start Recipe" a rota é alterada', async () => {
+    const { history } = renderWithRouterAndContextProvider(<App />);
+    act(() => {
+      history.push(endPoint);
+    });
+
+    await waitFor(() => {
+      expect(screen.getByTestId('favorite-btn')).toBeInTheDocument();
+    });
+    const startRecipeBTN = screen.getByRole('button', { name: /start recipe/i });
+    expect(startRecipeBTN).not.toHaveTextContent('Continue Recipe');
+    act(() => {
+      userEvent.click(startRecipeBTN);
+    });
+    expect(history.location.pathname).toBe('/drinks/178319/in-progress');
+    expect(localStorage.getItem('inProgressRecipes')).toBeTruthy();
+  });
+
+  // test('testando os botões "share" e "favorite"', async () => {
+  //   const { history } = renderWithRouterAndContextProvider(<App />);
+  //   act(() => {
+  //     history.push(endPoint);
+  //   });
+
+  //   await waitFor(() => {
+  //     expect(screen.getByRole('img', { name: /corba/i })).toBeInTheDocument();
+  //   });
+
+  //   const btnShare = screen.getByRole('button', { name: /share/i });
+  //   // const btnFavorite = screen.getByRole('button', { name: /share/i });
+  //   act(() => {
+  //     // userEvent.click(btnShare);
+  //   });
+  //   // await waitFor(() => expect(screen.getByText('Link copied!')).toBeInTheDocument());
+  // });
 });
